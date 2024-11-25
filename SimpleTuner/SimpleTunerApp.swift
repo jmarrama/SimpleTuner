db0d70a7b3b1ca0c55a89931afe8a6f1dc69bf3a
Joe Marrama <jmarrama@gmail.com>
2024-11-25 11:19:35 -0500

    Commit all starter code

---
+++ SimpleTuner/SimpleTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTunerApp.swift
+//  SimpleTuner
+//
+//  Created by Joe Marrama on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleTunerTests/SimpleTunerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTunerTests.swift
+//  SimpleTunerTests
+//
+//  Created by Joe Marrama on 11/25/24.
+//
+
+import Testing
+@testable import SimpleTuner
+
+struct SimpleTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
